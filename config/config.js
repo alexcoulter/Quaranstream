2546bd9c6c7fd02e48839e7d0ce7d670fc96785f
Alex Coulter <ALEXCOULTER.UNC@GMAIL.COM>
2020-04-10 17:11:30 -0400

    adding files

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: process.env.NAME,
+    password: process.env.PASSWORD,
+    database: "quaranstream_db",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    define: {
+      timestamps: false
+  }
+  },
+  test: {
+    username: process.env.NAME,
+    password: process.env.PASSWORD,
+    database: "quaranstream_db",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
+};
